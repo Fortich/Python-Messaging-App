b94a96b3192773a778da78744240afc21166e1bf
Fortich <ajsuarezf@unal.edu.co>
2020-06-01 15:20:41 -0500

    Fix linting in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, current_app
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 from dotenv import load_dotenv
 from subprocess import call
 import threading
@@ -13,9 +13,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
 @app.route('/')
 def hello_world():
     return render_template('socket.html')
+
 
 @app.route('/receive', methods=['POST'])
 def receive_message():
@@ -23,29 +25,32 @@ def receive_message():
     socketio.emit('new', content)
     return 'ok'
 
+
 def getToList():
     with open('toList.js') as json_file:
         data = json.load(json_file)
     print(data)
     return data
 
+
 @socketio.on('message')
 def handle_message(message):
+    sendurl = 'https://fcm.googleapis.com/fcm/send'
     headers = {
         'Authorization': 'key={}'.format(os.environ['ApiKey'])
     }
     json_string = {
         "registration_ids": getToList(),
-        "notification" : {
-            "title" : "",
-            "body" : str(message)
+        "notification": {
+            "title": "",
+            "body": str(message)
         }
     }
-    getToList()
-    res = requests.post('https://fcm.googleapis.com/fcm/send', headers=headers, json=json_string)
+    res = requests.post(sendurl, headers=headers, json=json_string)
     print(res)
 
+
 if __name__ == '__main__':
-    threading.Thread(target=socketio.run, args=(app,'localhost', os.environ['PORT'])).start()
-    call(["node", "index.js"]) 
-    
+    targs = (app, 'localhost', os.environ['PORT'])
+    threading.Thread(target=socketio.run, args=(targs)).start()
+    call(["node", "index.js"])
